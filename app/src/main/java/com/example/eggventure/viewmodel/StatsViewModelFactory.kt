d051edbfdbc899b5a67b26c9ad34e33768d5904c
KebabOnline <merek77777@gmail.com>
2025-05-21 20:08:57 +0200

    stats factory

---
+++ app/src/main/java/com/example/eggventure/viewmodel/StatsViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.eggventure.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.eggventure.model.repository.RunRepository
+
+class StatsViewModelFactory(private val runRepository: RunRepository) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(StatsViewModel::class.java)) {
+            return StatsViewModel(runRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
